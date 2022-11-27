b7ee334cc120a3d58502d2e3e4de78003d268cd4
Cole Sussmeier <colesussmeier@gmail.com>
2022-11-27 16:32:49 -0500

    lambda connects to rds

--- amplify/backend/function/retsfunction/src/index.js
+++ amplify/backend/function/retsfunction/src/index.js
@@ -1,20 +1,27 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
- exports.handler = async (event) => {
-    console.log(event)
-    const zip = event.pathParameters.zip;
-    const location = {'zip': zip, 'locationName': "location " + zip };
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-     headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers": "*"
-     }, 
-        body: JSON.stringify(location),
-    };
-    return response;
+
+var mysql = require('mysql');
+var creds = require('./rdsconfig.json');
+
+var pool = mysql.createPool({
+    host : creds.dbhost,
+    user : creds.dbuser,
+    password : creds.dbpassword,
+    database : creds.dbname
+});
+
+
+ exports.handler = function(event, context, callback) {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    pool.getConnection(function(err, connection) {
+        connection.query( "select * from zhvi_processed where ZipCode = '10516' limit 1;", function (error, results, fields){
+            connection.release();
+
+            if (error) callback(error);
+            else callback(null, results[0]);
+        });
+    });
 };
